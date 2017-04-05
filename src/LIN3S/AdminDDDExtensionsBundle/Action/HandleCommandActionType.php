6a5b3d414b088ab2ff4bbfc3d2a16406f1f127f0
benatespina <benatespina@gmail.com>
2017-04-05 08:48:27 +0200

    Replace lin3s admin's form handler with symfony form factory

--- src/LIN3S/AdminDDDExtensionsBundle/Action/HandleCommandActionType.php
+++ src/LIN3S/AdminDDDExtensionsBundle/Action/HandleCommandActionType.php
@@ -13,8 +13,8 @@ namespace LIN3S\AdminDDDExtensionsBundle\Action;
 
 use LIN3S\AdminBundle\Configuration\Model\Entity;
 use LIN3S\AdminBundle\Configuration\Type\ActionType;
-use LIN3S\AdminBundle\Form\FormHandler;
 use LIN3S\SharedKernel\Application\CommandBus;
+use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
@@ -28,18 +28,18 @@ final class HandleCommandActionType implements ActionType
 
     private $flashBag;
     private $twig;
-    private $formHandler;
+    private $formFactory;
     private $commandBus;
     private $urlGenerator;
 
     public function __construct(
-        FormHandler $formHandler,
+        FormFactoryInterface $formFactory,
         CommandBus $commandBus,
         \Twig_Environment $twig,
         FlashBagInterface $flashBag,
         UrlGeneratorInterface $urlGenerator
     ) {
-        $this->formHandler = $formHandler;
+        $this->formFactory = $formFactory;
         $this->commandBus = $commandBus;
         $this->twig = $twig;
         $this->flashBag = $flashBag;
@@ -50,7 +50,7 @@ final class HandleCommandActionType implements ActionType
     {
         $this->checkRequired($options, 'form');
 
-        $form = $this->formHandler->createForm($options['form'], $entity); //, ['resource_id' => $entity->id()]);
+        $form = $this->formFactory->create($options['form'], $entity);
         if ($request->isMethod('POST') || $request->isMethod('PUT') || $request->isMethod('PATCH')) {
             $form->handleRequest($request);
             if ($form->isValid() && $form->isSubmitted()) {
@@ -66,15 +66,14 @@ final class HandleCommandActionType implements ActionType
                 );
 
                 return $this->redirect($this->urlGenerator, $options, $config->name(), $form->getData());
-            } else {
-                $this->flashBag->add(
-                    'lin3s_admin_error',
-                    sprintf(
-                        'Errors while saving %s. Please check all fields and try again',
-                        $config->name()
-                    )
-                );
             }
+            $this->flashBag->add(
+                'lin3s_admin_error',
+                sprintf(
+                    'Errors while saving %s. Please check all fields and try again',
+                    $config->name()
+                )
+            );
         }
 
         return new Response(
